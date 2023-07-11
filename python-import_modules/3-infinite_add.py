1cc117a3c8dd6bc4e6ea581bafe26ffdfd3a82a7
Bfestus <f.bigiriman@alustudent.com>
2023-07-11 00:19:57 -0700

    task

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def add_arguments(*args):
+    total = sum(int(arg) for arg in args)
+    print(total)
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    add_arguments(*arguments)
