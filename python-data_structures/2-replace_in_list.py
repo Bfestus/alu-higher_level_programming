4a15b5aa85f7fc19033c55705957afb19aebc4d9
Bfestus <f.bigiriman@alustudent.com>
2023-07-15 13:10:16 -0700

    task

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+new_element = 9
+def replace_in_list(my_list, idx, element):
+    if (idx <0) or (idx >= len(my_list)):
+        return my_list
+replace_in_list(my_list, idx, element=new_element)    
+
