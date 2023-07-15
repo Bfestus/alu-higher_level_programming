50a61be034239123a1f82e48214087f18cb54b8e
Bfestus <f.bigiriman@alustudent.com>
2023-07-15 12:50:24 -0700

    tasks

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-  if idx = -10 :
-    return none
-  elif idx > my_list:
-    return none
+  if idx <0 or idx >= my_list:
+      return none 
+  return my_list[idx]
