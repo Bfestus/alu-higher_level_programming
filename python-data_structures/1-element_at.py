f9dd9b66da011b07feb6aafbf3e682a071754bbf
Bfestus <f.bigiriman@alustudent.com>
2023-07-15 13:00:54 -0700

    tasks

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-  if idx <0 or idx >= my_list:
+  if (idx <0) or (idx >= len(my_list)):
       return none 
-  return my_list[idx]
-print("Element at index 1 is", element_at(my_list, 1))
+  else:
+    return my_list[idx]
