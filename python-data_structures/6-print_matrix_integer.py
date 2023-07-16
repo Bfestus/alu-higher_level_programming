e77bb0ee66f04e70be21befc21dddff0c92e5f4b
Bfestus <f.bigiriman@alustudent.com>
2023-07-16 13:02:08 -0700

    tasks

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index,element in enumerate(row):
+            if index ==len(row) -1:
+                print({":d"}.format(element))
+            else:
+                print({:d}.format(element, end=" "))
+print_matrix_integer(matrix)
+print('--')
+print_matrix_integer()
