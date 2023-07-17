ba13db0640957871d40ecf38a367467628719c1f
Bfestus <f.bigiriman@alustudent.com>
2023-07-17 10:39:17 -0700

    tasks

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list - 1):
-        return my_list  # Return the same list if idx is negative or out of range
+    """deletes the item at a specific position
+    """
+    if idx < 0 or (idx > (len(my_list) - 1)):
+        return my_list
     else:
         del my_list[idx]
-        return my_list
+        return (my_list)
