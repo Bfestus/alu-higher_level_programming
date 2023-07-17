8fa1ad508a3169c96dfaceccb2c17b0c5fcc23b5
Bfestus <f.bigiriman@alustudent.com>
2023-07-17 11:02:25 -0700

    tasks

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@ my_list = ['1', '2', '3']
 
 
 def print_list_integer(my_list=[]):
-    for integer in  my_list:
+    for integer in my_list:
         print("{:d}".format(integer))
-    return my_list      
+    return my_list
