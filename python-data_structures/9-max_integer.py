f9d12d284e404811bd7e928557b8670923ef0e65
Bfestus <f.bigiriman@alustudent.com>
2023-07-17 12:40:21 -0700

    task

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,6 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    largest_integer = my_list[0]
-    for num in my_list:
-        if num > largest_integer:
-            largest_integer = num
-    return largest_integer         
+    else:
+        my_list.sort()
+        return my_list[-1]
