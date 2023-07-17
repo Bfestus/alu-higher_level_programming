3a21c25b1cbfad75526ee7a2d4f32f9ea8a8b041
Bfestus <f.bigiriman@alustudent.com>
2023-07-17 04:25:26 -0700

    tasks

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        print(0, none)
+        return(0, None)
     else:
-        print(len(sentence), sentence[0])
-
+        return(len(sentence), sentence[0])
