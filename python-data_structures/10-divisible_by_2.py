569b22b87027a0907b1bfd946070eb2bb5800971
Bfestus <f.bigiriman@alustudent.com>
2023-07-17 05:05:27 -0700

    tasks

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
+
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
-        if i % 2 == 0:
-            new_list += i
+        my_list.append(num % 2 == 0)
+            
     return new_list         
