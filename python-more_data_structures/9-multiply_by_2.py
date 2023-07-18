824a8c630d9c8fb00c2e730d80a4374d026e0439
Bfestus <f.bigiriman@alustudent.com>
2023-07-18 04:17:16 -0700

    tasks

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,7 @@
 def multiply_by_2(a_dictionary):
     new_dictionary ={}
     for key in a_dictionary:
-        print(i, ":", (my_dict[i] * 2))
+        new_data = (i, ":", (my_dict[i] * 2))
+        new_dictionary = new_data
+        print(new_dictionary)
     return a_dictionary    
