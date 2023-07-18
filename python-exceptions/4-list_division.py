5496db867b088e590beeea822296a7b1f3f15d37
Bfestus <f.bigiriman@alustudent.com>
2023-07-18 14:08:06 -0700

    tasks

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    try:
+        for i in range(list_length):
+            try:
+                element1 = my_list_1[i]
+                element2 = my_list_2[i]
+                result = element1 / element2
+            except TypeError:
+                print("wrong type")
+                result = 0
+            except ZeroDivisionError:
+                print("division by 0")
+                result = 0
+            except IndexError:
+                print("out of range")
+                result = 0
+            finally:
+                result_list.append(result)
+    finally:
+        return result_list
