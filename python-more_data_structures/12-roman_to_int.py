6030dd8849dee824edfb3c2f243a160a864e2879
Bfestus <f.bigiriman@alustudent.com>
2023-07-18 13:27:14 -0700

    tasks

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
     roman_numerals = {
         'I': 1,
         'V': 5,
@@ -11,13 +12,16 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
+
     result = 0
     previous_value = 0
+
     for char in reversed(roman_string):
         value = roman_numerals[char]
         if value >= previous_value:
-            result = result + value
+            result += value
         else:
-            result = result - value
-            previous_value = value
+            result -= value
+        previous_value = value
+
     return result
