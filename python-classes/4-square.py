3b6f4722805ef9936f1741b3e4aa7683840709c0
Bfestus <f.bigiriman@alustudent.com>
2023-07-24 03:43:07 -0700

    python class

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,5 +21,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    
     def area(self):
         return self.__size ** 2    
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,10 +7,12 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new square."""
         self.size = size
+    """defining the getter"""
     @property
     def size(self):
         """To get the size"""
         return self.__size
+    """ defining the setter"""
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -22,7 +24,7 @@ class Square:
         return self.__size ** 2
     def my_print(self):
         if self.__size == 0:
-            print()# empty line 
+            print()  # empty line 
         else:
             for i in range(self.__size):
                 print("#" * self.__size)
