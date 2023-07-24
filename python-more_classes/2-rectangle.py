cde420f52b901000362796fce9d4e742179ab04b
Bfestus <f.bigiriman@alustudent.com>
2023-07-24 06:49:57 -0700

    tasks

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" creating the empty class"""
+
+
+class Rectangle:
+    """defining rectange"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    """defining a getter, width"""
+    @property
+    def width(self):
+        return self.__width
+    """defining a setter, width"""
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    """defining a getter, height"""
+    @property
+    def height(self):
+        return self.__height
+    """defining a setter"""
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    """defining area self"""
+    def area(self):
+       return  self.__height * self.__width
+   """defining perimeter """
+   def perimeter(self):
+       return (self.__width + self.height) * 2
+
