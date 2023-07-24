4452a7b061f0e18b1e9ec18d2a113aefe768f2a8
Bfestus <f.bigiriman@alustudent.com>
2023-07-23 22:56:50 -0700

    python class

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""creating a class square."""
+class Square:
+    """initializing a square object."""
+    def __init__(self, size):
+        """creating the instance attribute size"""
+        self.size = size
