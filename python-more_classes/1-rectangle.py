2fa3c3c839b67797d9cbcc06122c8c68c38bd13b
Bfestus <f.bigiriman@alustudent.com>
2023-07-24 06:05:42 -0700

    tasks

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,14 +3,15 @@
 
 
 class Rectangle:
+    """defining rectange"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
+    """defining a getter, width"""
     @property
     def width(self):
         return self.__width
-
+    """defining a setter, width"""
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -18,11 +19,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
+    """defining a getter, height"""
     @property
     def height(self):
         return self.__height
-
+    """defining a setter"""
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
