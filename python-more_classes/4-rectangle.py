38e79016ceb222fd12f5c2227f7392f03c0561c8
Bfestus <f.bigiriman@alustudent.com>
2023-07-25 06:58:35 -0700

    tasks

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -78,7 +78,8 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return "\n".join(["#" * self.width] * self.height)   
+            return "\n".join(["#" * self.width] * self.height)
+    
     def __repr__(self):
         """Return a string representation of the Rectangle.
 
