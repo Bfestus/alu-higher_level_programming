b6ea4192eebee1e518d45cbc59fa2d051b8cbe12
Bfestus <f.bigiriman@alustudent.com>
2023-07-26 08:15:46 -0700

    tasks

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n):
-    return ''.join(['BestSchool' + str(i) for i in range(1, n + 1)])
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "Holberton" + ", Holberton" * count[0]
