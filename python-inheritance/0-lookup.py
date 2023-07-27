d0ec71947e98f548bc78f9ded884128f35843c56
Bfestus <f.bigiriman@alustudent.com>
2023-07-27 11:26:58 -0700

    tasks

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """define a class """
+
+
 def lookup(obj):
     """ returns the list of available attributes and methods of an object:
     we use dir()to get a list of attributs and methodes"""
