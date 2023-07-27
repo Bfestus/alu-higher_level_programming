a2291428e7f5e142a0e7053410e48d1c196f800c
Bfestus <f.bigiriman@alustudent.com>
2023-07-27 05:10:44 -0700

    tasks

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" define the class"""
+class MyList(list):
+    """A subclass of list"""
+    def __init__(self)
+        """Initializes the object"""
+        super().__init__():
+
+    def print_sorted(self):
+          """Prints the sorted list"""
+        sorted_list = sorted(self)
+        for item in sorted_list:
+            print(item)
+        print()
