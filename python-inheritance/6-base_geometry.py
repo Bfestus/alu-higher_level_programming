0ff0065db9547862c00ccb56b290c13f443e16d1
Bfestus <f.bigiriman@alustudent.com>
2023-07-27 06:53:35 -0700

    task

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-Contains the class BaseGeometry """
+Contains the class BaseGeometry
+"""
 
 
-def BaseGeometry:
-    """initializing the class BaseGeometry"""
-    raise Exception("area() is not implemented")
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
