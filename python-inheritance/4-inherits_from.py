29ed650109d7656a43a944602d0b7d5412d59e14
Bfestus <f.bigiriman@alustudent.com>
2023-07-27 06:50:28 -0700

    tasks

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ This module contains the function is_same_class
 def inherits_from(obj, a_class):
     """if the object is an instance of a class that inherited 
     (directly or indirectly) from the specified class """
-    return issubclass(obj, a_class)
+    return isinstance(obj, object) and issubclass(type(obj), a_class) and type(obj) != a_class
