45218452ce85265e00932e4af1a0f295d4027683
Bfestus <f.bigiriman@alustudent.com>
2023-07-27 13:47:27 -0700

    tasks

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,11 +5,42 @@ contains the MyList class
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+     """
+    A custom list class that inherits from the built-in list class.
+
+    Methods:
+        print_sorted(): Prints the elements of the list in ascending order.
+    """
 
     def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+        """
+        Prints the elements of the list in ascending order.
+        Does not modify the original list.
+
+        Example:
+            my_list = MyList()
+            my_list.append(1)
+            my_list.append(4)
+            my_list.append(2)
+            my_list.append(3)
+            my_list.append(5)
+
+            print(my_list)  # Output: [1, 4, 2, 3, 5]
+            my_list.print_sorted()  # Output: [1, 2, 3, 4, 5]
+            print(my_list)  # Output: [1, 4, 2, 3, 5]
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
+
+    def __str__(self):
+        """
+        Returns a string representation of the list.
+
+        Returns:
+            str: A string representing the elements of the list.
+
+        Example:
+            my_list = MyList([1, 2, 3, 4, 5])
+            print(my_list)  # Output: "[1, 2, 3, 4, 5]"
+        """
+        return super().__str__()
