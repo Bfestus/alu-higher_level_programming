a54198aa1ad884952ae1eb0046004c6430c97368
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 04:19:19 -0700

    task

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for Write_file method.
+"""
+def write_file(filename="", text=""):
+    """
+    Write a string to the text file
+    """
+    with open(filename, 'w', encoding="UTF-8") as file:
+        """
+        Writing the a string to  text file
+        """
+        text = file.write()
+        return text
