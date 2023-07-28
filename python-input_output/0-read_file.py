e56f29af21592759879794f472521e3f6eb3cac8
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 04:06:39 -0700

    tasks

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """
-Module for read_file method.
+Module: file_reader
+Description: A module to read and print the content of a text file.
 """
 
 
 def read_file(filename=""):
     """
-    Reads text file and prints to STDOUT
+    Read and print the content of the specified text file.
+
+    Parameters:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
     """
-    with open(filename, 'r', encoding="UTF-8") as file:
-        """
-        reading the contend
-        """
+    if not filename:
+        print("Error: No filename provided.")
+        return
+    
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
