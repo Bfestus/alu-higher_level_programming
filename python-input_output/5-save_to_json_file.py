3b00062e8af1dfe41b28af64da3a24fefd111552
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 07:45:19 -0700

    tasks

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation:"""
     with open(filename, 'w') as file:
-        json.jump(my_obj, file)
+        json.dump(my_obj, file)
 
