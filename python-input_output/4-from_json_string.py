9d5577c78804dcaa8eb3cea390653c210b5da909
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 07:23:24 -0700

    tasks

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module from_json_string method.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object (Python data structure) represented by a JSON string
+    firstly we have to serialize object string
+    """
+    return (json.loads(my_obj))
