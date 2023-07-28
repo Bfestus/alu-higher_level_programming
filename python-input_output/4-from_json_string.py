3f64dd56f96603c25c3cd9cbbc808468cc5248ec
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 07:25:17 -0700

    tasks

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -12,4 +12,4 @@ def from_json_string(my_str):
     returns an object (Python data structure) represented by a JSON string
     firstly we have to serialize object string
     """
-    return (json.loads(my_obj))
+    return (json.loads(my_str))
