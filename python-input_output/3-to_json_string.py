feffa1857ccb47bb214e8b642263091cc43aa39f
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 05:46:11 -0700

    tasks

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -11,9 +11,4 @@ def to_json_string(my_obj):
     returns the JSON representation of an object (string)
     firstly we have to serialize object string
     """
-    with open(my_obj, 'w') as file:
-        """returns the JSON representation of an object"""
-        json_represantation =  json.dump(my_obj, file)
-        return json_represantation
-    
-
+    return (json.dumps(my_obj))
