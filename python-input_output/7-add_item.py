13058ad282049f0d448d82dcd3a1ef2dd3a7a526
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 08:39:07 -0700

    tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""
-    Python script that adds all args to a Python List.
-    List is then saved to a file.
-"""
-
-
+'''script adds all arguments to a python list
+'''
 import sys
 import json
-import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
-
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
-
-save_to_json_file(json_list, file)
+try:
+    json_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    json_list = []
+json_list += sys.argv[1:]
+save_to_json_file(json_list, "add_item.json")
