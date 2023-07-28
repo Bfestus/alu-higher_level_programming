43d5621bd2a289944465696b9b781243eb7c2c8d
Bfestus <f.bigiriman@alustudent.com>
2023-07-28 08:51:16 -0700

    tasks

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,8 +2,10 @@
 """
     Module for class Student.
 """
+
+
 class Student:
-     """
+    """
         A class students that defines a student by:
         Attributes:
             first_name (str): name of student.
@@ -20,8 +22,9 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
-         """
+        """
             retrieves a dictionary representation of Student.
         """
         return self.__dict__
