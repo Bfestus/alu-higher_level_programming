6ff87c9801d57addbd659887884b555a51a10028
Bfestus <f.bigiriman@alustudent.com>
2023-08-02 13:49:48 -0700

    tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- My privileges!
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show privileges
+-- Show privileges for `user_0d_1`
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show privileges for `user_0d_2`
+SHOW GRANTS FOR user_0d_2@localhost;
