c5b524e821a099cc0f65ccbd4441745504333ab6
Bfestus <f.bigiriman@alustudent.com>
2023-08-02 02:36:19 -0700

    tasks

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table second_table
--- display result in order
+-- List all records of the `second_table` by `score` in descending order
 SELECT score, name FROM `second_table` ORDER BY score desc;
