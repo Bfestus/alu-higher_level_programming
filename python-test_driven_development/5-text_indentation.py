949cd1f8270295a4a98a1115e72929935ce9328b
Bfestus <f.bigiriman@alustudent.com>
2023-08-13 10:03:23 -0700

    task

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,5 +12,17 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    str = str.replaceAll("(\\p{Punct})\\s?", "$1\n");
-    print(str)
+    list_punctuation = [".", "?", ":"]
+    lines = []
+    current_line = ""
+    for char in text:
+        current_line += char
+        if char in punctuation:
+            lines.append(current_line.strip())
+            current_line = ""
+    
+    if current_line.strip():
+        lines.append(current_line.strip())
+    
+    for line in lines:
+        print(line + "\n\n", end='')        
