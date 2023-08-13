e45aa2257c875f11d0f1da7e654113e79c612a7d
Bfestus <f.bigiriman@alustudent.com>
2023-08-12 22:52:04 -0700

    tasks

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -13,21 +13,21 @@ def matrix_divided(matrix, div):
     if not (type(matrix) == list) or not all(type(row) == list for row in matrix) or \
        not all(type(num) in (int, float) for row in matrix for num in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+
     # Check if each row of the matrix has the same size
     row_lengths = set(len(row) for row in matrix)
     if len(row_lengths) > 1:
         raise TypeError("Each row of the matrix must have the same size")
-    
+
     # Check if div is a number (integer or float)
     if not (type(div) in (int, float)):
         raise TypeError("div must be a number")
-    
+
     # Check if div is not equal to zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     # Divide each element of the matrix by div and round to 2 decimal places
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
-    
+
     return new_matrix
