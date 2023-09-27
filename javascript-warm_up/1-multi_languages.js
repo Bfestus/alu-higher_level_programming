ee33a4acb921a4bce662efc677121c28821ad4ad
Bfestus <f.bigiriman@alustudent.com>
2023-09-27 05:50:30 -0700

    bug fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
+/* a script that prints “JavaScript is amazing” */
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
