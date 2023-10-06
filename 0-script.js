f2f58f4e1dfafaf495863a7b4299f8996d8340bc
Bfestus <127530529+Bfestus@users.noreply.github.com>
2023-10-06 14:10:50 +0200

    Update 0-script.js

--- 0-script.js
+++ 0-script.js
@@ -1,5 +1,1 @@
-// Get the <header> element using document.querySelector
-var headerElement = document.querySelector("header");
 
-// Update the text color to red
-headerElement.style.color = "#FF0000";
